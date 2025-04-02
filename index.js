5761a50c2bf6d05895fea03700d6d012177c635f
abc <abc@.com>
2025-04-02 12:20:31 +0530

    02/April Deletedata

--- index.js
+++ index.js
@@ -2,22 +2,42 @@ let input = document.getElementById("inp")
 let btn = document.getElementById("btn")
 let list = document.getElementById("list")
 let todoArr = []
+let id = 1;
+getData();
+
+
+
 
 function showTask(data){
     let li = document.createElement("li")
 
+    li.addEventListener("click" , (event)=>{
+        let localdata = JSON.parse(localStorage.getItem("todo"))
+        let filterData = localdata.filter((task)=>{
+           
+            return task.taskId != data.taskId
+        })
+
+        localStorage.setItem("todo" ,JSON.stringify(filterData) )
+
+        list.removeChild(event.target)
+    })
+
+
     li.innerText = data.task;
     list.appendChild(li);
 }
-getData();
 
-let id = 1;
 
 function getData(){
     let data = JSON.parse(localStorage.getItem("todo"))
 
     todoArr = data || [];
-
+    if(todoArr.length){
+        id = todoArr[todoArr.length-1].taskId
+        id++;
+    
+    }
     todoArr.forEach((v)=>{
 
         showTask(v)
@@ -29,6 +49,7 @@ function getData(){
 btn.addEventListener("click",(e)=>{
     let data = {
         task: input.value,
+        taskId  : id
     }
 
     todoArr.push(data);
@@ -39,6 +60,7 @@ btn.addEventListener("click",(e)=>{
     showTask(data)
 
     input.value = ""
+    id++;
 
 })
 
