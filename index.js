52b2f736b7687826cdb945cdf638d2cd34c7a54e
abc <abc@.com>
2025-04-16 14:52:42 +0530

    16 April Callback and callback hell

--- index.js
+++ index.js
@@ -6,42 +6,57 @@ let pauseBtn = document.getElementById("pause")
 let secCount = 0;
 let intervalId = 0;
 
+function selectProduct(callback){
 
-for(var i = 0 ; i < 5 ; i++){
     setTimeout(()=>{
-        console.log(i);
-    } , 2000);
+        let products = ["car" , "bike"]
+        console.log(products)
+            callback(products);
+
+    },1000)
+}
+
+
+function createOrder(products , callback ){
+    setTimeout(()=>{
+
+        let orderId = 1234
+        console.log("order for products" , products)
+        callback(orderId)
+    },1000)
+
+}
+
+function makePayment(orderId , callback){
+
+    setTimeout(()=>{
+
+        let paymentInfo = 987
+        console.log("orderId" , orderId)
+        callback(paymentInfo)
+    },1000)
+
+}
+
+function orderComplete(paymentInfo){
+    setTimeout(()=>{
+        console.log("order Complete paymentInfo" , paymentInfo);
+
+    },1000)
 
 }
 
 
 
+selectProduct(function(products){
+        createOrder(products , function(orderId){
+            makePayment(orderId , function(paymentInfo){
+                orderComplete(paymentInfo)
+
+            })
+
+        })
 
 
-
-
-
-// startBtn.addEventListener("click",()=>{
-//     intervalId = setInterval(()=>{
-//         sec.innerText = secCount
-//         secCount++;
-//     },1000)
-// })
-
-// pauseBtn.addEventListener("click",()=>{
-//   clearInterval(intervalId)
-//   sec.innerText = "00"
-// secCount = 0;
-// })
-
-
-
-
-
-
-
-
-
-
-
+})
 
