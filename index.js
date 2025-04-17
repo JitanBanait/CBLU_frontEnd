5cdc3025b8622adaa31305fa62908ad542f57276
abc <abc@.com>
2025-04-17 12:25:17 +0530

    17 April Promises

--- index.js
+++ index.js
@@ -6,57 +6,92 @@ let pauseBtn = document.getElementById("pause")
 let secCount = 0;
 let intervalId = 0;
 
+
+
+
+
+
+
+
+
+
+
 function selectProduct(callback){
 
-    setTimeout(()=>{
-        let products = ["car" , "bike"]
-        console.log(products)
-            callback(products);
-
-    },1000)
+return new Promise((resolve, reject) => {
+    try {
+        setTimeout(()=>{
+            let products = ["car" , "bike"]
+            console.log(products)
+           resolve(products);
+    
+        },1000)
+        
+    } catch (error) {
+        reject(error)
+    }
+})
 }
 
-
 function createOrder(products , callback ){
-    setTimeout(()=>{
 
-        let orderId = 1234
-        console.log("order for products" , products)
-        callback(orderId)
-    },1000)
+   return new Promise((resolve, reject) => {
+        
+        setTimeout(()=>{
+    
+            let orderId = 1234
+            console.log("order for products" , products)
+            resolve(orderId)
+        },1000)
+    })
 
 }
 
 function makePayment(orderId , callback){
 
-    setTimeout(()=>{
 
-        let paymentInfo = 987
-        console.log("orderId" , orderId)
-        callback(paymentInfo)
-    },1000)
+  return  new Promise((resolve, reject) => {
+        
+        setTimeout(()=>{
+    
+            let paymentInfo = 987
+            console.log("orderId" , orderId)
+            resolve(paymentInfo)
+        },1000)
+    })
 
 }
 
 function orderComplete(paymentInfo){
-    setTimeout(()=>{
-        console.log("order Complete paymentInfo" , paymentInfo);
 
-    },1000)
+   return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log("order Complete paymentInfo" , paymentInfo);
+    
+        },1000)
+        
+    })
 
 }
 
-
-
-selectProduct(function(products){
-        createOrder(products , function(orderId){
-            makePayment(orderId , function(paymentInfo){
-                orderComplete(paymentInfo)
-
-            })
-
-        })
-
-
+selectProduct().then((products)=>{
+   return createOrder(products)
+}).then((orderId)=>{
+   
+   return makePayment(orderId)
+}).then((paymentInfo)=>{
+    orderComplete(paymentInfo)
 })
+
+// selectProduct(function(products){
+//         createOrder(products , function(orderId){
+//             makePayment(orderId , function(paymentInfo){
+//                 orderComplete(paymentInfo)
+
+//             })
+
+//         })
+
+
+// })
 
